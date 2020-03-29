7bd7c7eabaae7fc20b5557018ccc3f95c318964f
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-03-29 19:07:30 +0530

    Refactored auth service

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 dotenv.config();
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {}
-  )
-);
+require("./services/auth");
 
 app.use("/auth", require("./routes/auth"));
 
