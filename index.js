6b54a8242e4f77141f784d143ed669ec75ffb9d8
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-03-29 18:53:01 +0530

    Scaffolded OAuth strategy

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ data: "Hello, friend." });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {}
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 app.listen(PORT, () => {
