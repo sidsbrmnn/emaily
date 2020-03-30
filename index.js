2d2f3e0c05670495128a4bdcf7f31c142fd0b168
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-03-30 16:32:37 +0530

    Moved database connection to index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 require("./services/error");
 require("./services/config");
 
 const app = express();
 
-require("./services/db");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/emaily";
+mongoose
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  });
+
 require("./services/auth");
 require("./services/routes")(app);
 
