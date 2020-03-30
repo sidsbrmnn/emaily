31099f1de0d56c9639206cacb3e16ff0e85c1b81
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-03-30 16:33:20 +0530

    Added SIGTERM handler

--- index.js
+++ index.js
@@ -23,6 +23,24 @@ require("./services/auth");
 require("./services/routes")(app);
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log("Listening on port:", PORT);
+});
+
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Server closed");
+    mongoose.connection.close((err) => {
+      if (err) {
+        throw err;
+      }
+
+      console.log("Disconnected from MongoDB");
+      process.exit(0);
+    });
+  });
 });
