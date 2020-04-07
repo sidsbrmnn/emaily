2092d4c66ed23f99780b8629f978754413805725
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-04-07 20:13:33 +0530

    Removed layour from pages

--- client/src/pages/surveys.js
+++ client/src/pages/surveys.js
@@ -1,13 +1,7 @@
 import React from "react";
 
-import Layout from "../components/Layout";
-
 const SurveysPage = () => {
-  return (
-    <Layout>
-      <h2>Surveys</h2>
-    </Layout>
-  );
+  return <h2>Surveys</h2>;
 };
 
 export default SurveysPage;
