1762167de42ebeaa141add43b1c50e1c8d26cb6d
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-04-07 20:15:33 +0530

    Modifed home page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const IndexPage = () => {
-  return <h2>Home</h2>;
+  return (
+    <section className="px-6 py-16">
+      <h2 className="mb-8">Home</h2>
+      <a
+        href="/auth/google"
+        className="p-4 rounded shadow-md text-sm bg-blue-600 text-gray-100 hover:shadow-inner focus:outline-none focus:shadow-inner"
+      >
+        Login with Google
+      </a>
+    </section>
+  );
 };
 
 export default IndexPage;
